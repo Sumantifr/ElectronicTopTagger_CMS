34bf15febe43983b7bdebee6fb0d3b7b311c7b5b
Sumantifr <s7384705218@gmail.com>
2021-06-12 15:35:07 +0200

    Adding UL cfg py file

--- CMSSW_10_6_20/src/Analysis/MC/test/crab_submit_Zprimejobs.sh
+++
@@ -1,4 +0,0 @@
-crab submit -c crabConfig_MC_JERCL5_ZptoTTBar_M2500_W25.py
-crab submit -c crabConfig_MC_JERCL5_ZptoTTBar_M2500_W250.py
-crab submit -c crabConfig_MC_JERCL5_ZptoTTBar_M5000_W50.py
-crab submit -c crabConfig_MC_JERCL5_ZptoTTBar_M5000_W500.py
